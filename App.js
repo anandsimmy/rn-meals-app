930fd67d6e7575ab59605caf4dbd7cb2a9a2e773
Anand Simmy <anandsimmy7@gmail.com>
2022-06-29 08:33:37 +0530

    minor changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Meals App</Text>
-      <StatusBar style='auto' />
+      <StatusBar />
     </View>
   );
 }
