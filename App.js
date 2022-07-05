d193988b5adfb951953407e1152edc10aaca959b
Anand Simmy <anandsimmy7@gmail.com>
2022-07-05 10:06:07 +0530

    minor changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meals App</Text>
+      <Text>Meals App Navigation</Text>
       <StatusBar style='auto' />
     </View>
   );
