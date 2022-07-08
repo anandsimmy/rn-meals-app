aa554a48cf02e5d890bad4e11f25d50efba59319
Anand Simmy <anandsimmy7@gmail.com>
2022-07-08 08:21:16 +0530

    added categories screen

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import { FlatList } from 'react-native';
+import CategoriesGridTile from '../Components/CategoriesGridTile';
+import { CATEGORIES } from '../data/dummy-data';
 
-const CategoriesScreen = () => {};
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <CategoriesGridTile {...item} />}
+      numColumns={2}
+    />
+  );
+};
 
 export default CategoriesScreen;
