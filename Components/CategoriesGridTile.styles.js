71c6ca79d4655f374f4058d0444a12b2e8b735d8
Anand Simmy <anandsimmy7@gmail.com>
2022-07-14 08:18:21 +0530

    style changes

---
+++ Components/CategoriesGridTile.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  gridTileContainer: {
+    height: 150,
+    width: '50%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  foodText: {
+    fontWeight: '500',
+  },
+});
