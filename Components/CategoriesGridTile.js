d28534de768453160e7047d75da7166af9f2dd0e
Anand Simmy <anandsimmy7@gmail.com>
2022-07-25 08:46:40 +0530

    minor changes

--- Components/CategoriesGridTile.js
+++ Components/CategoriesGridTile.js
@@ -3,7 +3,6 @@ import { View, Text } from 'react-native';
 import Styles from './CategoriesGridTile.styles';
 
 const CategoriesGridTile = ({ title, color }) => {
-  console.log(title, color);
   return (
     <View style={[Styles.gridTileContainer, { backgroundColor: color }]}>
       <Text style={Styles.foodText}>{title}</Text>
